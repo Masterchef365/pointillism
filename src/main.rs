66b918a6f23d31d68fecb98c3ed18ce401d27570
Masterchef365 <duncan.freeman1@gmail.com>
2022-03-10 01:30:41 -0800

    First draft

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use anyhow::{Context, Result};
+use rand::prelude::*;
+use rand::SeedableRng;
 use std::fs::File;
 use std::io::BufWriter;
 use std::ops::{Add, Sub};
@@ -16,15 +18,15 @@ struct Opt {
     output: PathBuf,
 
     /// Number of points to attempt
-    #[structopt(short, long, default_value = "10000")]
+    #[structopt(short, long, default_value = "100000")]
     n_points: usize,
 
     /// Minimum point size
-    #[structopt(long = "minp", default_value = "3")]
+    #[structopt(long = "pmin", default_value = "3")]
     min_point_size: isize,
 
     /// Minimum point size
-    #[structopt(long = "maxp", default_value = "10")]
+    #[structopt(long = "pmax", default_value = "10")]
     max_point_size: isize,
 
     /// Random seed (unsigned integer)
@@ -33,20 +35,70 @@ struct Opt {
 }
 
 fn main() -> Result<()> {
+    // Parse args
     let args = Opt::from_args();
 
+    // Read image
     let input_img = read_png(&args.input).context("Failed to read input image")?;
 
+    // Create output image
     let mut output_img = RgbImage::new(input_img.width(), input_img.height());
 
-    let center = Coord(output_img.width() as isize / 2, output_img.height() as isize / 2);
+    // Seed
+    let seed = args.seed.unwrap_or_else(|| rand::thread_rng().gen());
+    let mut rng = rand::rngs::SmallRng::seed_from_u64(seed);
+    println!("Using seed {}", seed);
 
-    let r = output_img.width().min(output_img.height()) as isize / 2;
-    for coord in fill_circle(r).map(|c| center + c) {
-        output_img.set(coord, [0xff, 0x00, 0x00]);
+    // Draw points
+    'points: for _ in 0..args.n_points {
+        // Sample a random pixel from the input image 
+        let sample_pos = random_pos(&mut rng, input_img.width(), input_img.height());
+        let color = input_img.get(sample_pos).unwrap();
+
+        // Select a random radius and point position in the input image
+        let r = rng.gen_range(args.min_point_size..=args.max_point_size);
+        let point_pos = random_pos(&mut rng, input_img.width(), input_img.height());
+
+        let fill_coords = || fill_circle(r).map(|c| point_pos + c);
+
+        // Calculate the score for this dot
+        let mut total_score_before = 0.0;
+        let mut total_score_after = 0.0;
+        for coord in fill_coords() {
+            let input_px = match input_img.get(coord) {
+                Some(px) => px,
+                None => continue 'points,
+            };
+
+            let output_px = output_img.get(coord).unwrap();
+
+            total_score_before += score(input_px, output_px);
+            total_score_after += score(input_px, color);
+        }
+
+        if total_score_after > total_score_before {
+            for coord in fill_coords() {
+                output_img.set(coord, color);
+            }
+        }
     }
 
+    // Write output image
     write_png(&args.output, &output_img)
+}
+
+fn random_pos(rng: &mut impl Rng, width: usize, height: usize) -> Coord {
+    Coord(
+        rng.gen_range(0..width) as isize,
+        rng.gen_range(0..height) as isize,
+    )
+}
+
+fn score(truth: [u8; 3], sample: [u8; 3]) -> f32 {
+    // Naive! use a better algo...
+    let [tr, tg, tb] = truth;
+    let [sr, sg, sb] = sample;
+    ((tr * sr + tg * sg + tb * sb) as f32).sqrt()
 }
 
 fn read_png(path: &PathBuf) -> Result<RgbImage> {
@@ -106,9 +158,9 @@ impl RgbImage {
         b.then(|| (x + y * self.width) * 3)
     }
 
-    pub fn get(&self, coord: Coord) -> [u8; 3] {
-        let idx = self.calc_index(coord).unwrap();
-        [self.data[idx + 0], self.data[idx + 1], self.data[idx + 2]]
+    pub fn get(&self, coord: Coord) -> Option<[u8; 3]> {
+        self.calc_index(coord)
+            .map(|idx| [self.data[idx + 0], self.data[idx + 1], self.data[idx + 2]])
     }
 
     pub fn set(&mut self, coord: Coord, [r, g, b]: [u8; 3]) {
